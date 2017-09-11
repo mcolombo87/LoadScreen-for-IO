146ad607ec4c80bfc9dedff8092b3192c73a4740
Maxi <mcolombo87@gmail.com>
2017-09-10 21:48:03 -0300

    End program for load OI in windows with a splash.

---
+++ OI for OpenOrange/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace OI_for_OpenOrange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 101);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(308, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Installer";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 171);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(621, 33);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(649, 238);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OI for OpenOrange";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ OI for OpenOrange/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace OI_for_OpenOrange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowDialog();
+        }
+
+        private void loading()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            progressBar1.PerformStep();
+            string argumentTest = "";
+            Boolean notShowSpash = false;
+            foreach (string arg in args)
+            {
+                argumentTest = arg;
+            }
+            //Process to communicate with OrangeInstaller in Python.
+            Process myProcess = new Process();
+
+            //myProcess.StartInfo.Verb ="runas";
+            switch (argumentTest)
+            {
+                case "--console":
+                    myProcess.StartInfo.FileName = ".\\OI.exe";
+                    myProcess.StartInfo.Arguments = "--console";
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.StartInfo.UseShellExecute = true;
+                    notShowSpash = true;
+                    break;
+                case "-C":
+                    myProcess.StartInfo.FileName = ".\\OI.exe";
+                    myProcess.StartInfo.Arguments = "--console";
+                    myProcess.StartInfo.CreateNoWindow = false;
+                    myProcess.StartInfo.UseShellExecute = false;
+                    notShowSpash = true;
+                    break;
+                case "--debug":
+                    myProcess.StartInfo.FileName = ".\\OI.exe";
+                    myProcess.StartInfo.CreateNoWindow = false;
+                    notShowSpash = true;
+                    break;
+                case "--test":
+                    break;
+                default:
+                    myProcess.StartInfo.FileName = ".\\OrangeInstaller.exe";
+                    myProcess.StartInfo.CreateNoWindow = false;
+                    myProcess.StartInfo.UseShellExecute = true;
+                    break;
+            }
+
+            Boolean err = false;
+            int timeOut = 0;
+            int lines;
+            try
+            {
+                lines = File.ReadLines(".\\install.log").Count();
+            }
+            catch
+            {
+                lines = 0;
+            }
+            myProcess.Start();
+            if (notShowSpash == true)
+            {
+                this.terminate();
+            }
+            while (err == false && timeOut < 50)
+            {
+                progressBar1.PerformStep();
+                this.Update();
+                try
+                {
+                    //var file = new FileStream("install.log", FileMode.Open, FileAccess.Read);
+                    while (lines == File.ReadLines(".\\install.log").Count() && progressBar1.Value < 100)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        timeOut += 1;
+                    }
+                }
+                catch
+                {
+                    err = true;
+                }
+            }
+            this.Update();
+            if (progressBar1.Value < 100)
+            {
+                while (progressBar1.Value < 100)
+                {
+                    progressBar1.PerformStep();
+                    this.Update();
+                }
+
+                //while (progressBar1.Value < 100)
+                //{
+                    //if (progressBar1.Value < 20)
+                    //{
+                    //    progressBar1.Value += 75;
+                    //}
+                    //else if (progressBar1.Value >= 20 && progressBar1.Value < 40)
+                    //{
+                    //    progressBar1.Value += 50;
+                    //}
+                    //else if (progressBar1.Value >= 40 && progressBar1.Value < 60)
+                    //{
+                    //    progressBar1.Value += 40;
+                    //}
+                    //else if (progressBar1.Value >= 60 && progressBar1.Value < 90)
+                    //{
+                    //    progressBar1.Value += 10;
+                    //}
+                    //else if (progressBar1.Value > 90)
+                    //{
+                    //    progressBar1.Value = 100;
+                    //    break;
+                    //}
+                //}
+            }
+
+            this.terminate();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            loading();
+        }
+
+        private void terminate()
+        {
+            this.Close();
+            this.Dispose();
+        }
+    }
+
+}
